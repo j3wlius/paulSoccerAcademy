951e6d48bf640031f463ac42491f253578eb009a
Julio <tamalejulius5@gmail.com>
2024-06-11 16:02:00 +0100

    update

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+        });
+        link.classList.add("active");
+    });
+});
